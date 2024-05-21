185255219d0818c7c0355f0f16e4624ad16d689c
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-05-21 12:18:17 +0000

    data types

---
+++ 02_data_types/index.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // int 
+    // long
+    // long long
+
+    // float
+    // double
+
+    // string
+    string s1;
+    getline(cin, s1);
+    cout << s1;
+
+
+    // char
+    return 0;
+}
