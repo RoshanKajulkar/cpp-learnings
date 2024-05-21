98d22b3e7fcd284d32963a64781876aff3dae312
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-05-21 12:03:40 +0000

    input output

---
+++ 01_input_output/index.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+
+#include<bits/stdc++.h>  // shortcut to include all commonly used libraries at once
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cin >> num;
+
+    cout << "Hey, Roshan!\n" << num;
+
+    return 0;
+}
+
